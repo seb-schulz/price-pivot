173409bf2d1545a5e7741a13d25b97f48689c620
Sebastian Schulz <16666034+seb-schulz@users.noreply.github.com>
2024-06-10 17:17:03 +0200

    Replace renovate token by separate token

--- scripts/check-updates.sh
+++ scripts/check-updates.sh
@@ -2,4 +2,4 @@
 
 set -xeu
 
-${DOCKER_BIN} run --env RENOVATE_REPOSITORIES --env RENOVATE_ONBOARDING=false --env RENOVATE_FORCE --env LOG_LEVEL --env RENOVATE_TOKEN=${GITHUB_TOKEN} --volume /tmp:/tmp --rm ghcr.io/renovatebot/renovate:latest
+${DOCKER_BIN} run --env RENOVATE_REPOSITORIES --env RENOVATE_ONBOARDING=false --env RENOVATE_FORCE --env LOG_LEVEL --env RENOVATE_TOKEN --volume /tmp:/tmp --rm ghcr.io/renovatebot/renovate:latest
